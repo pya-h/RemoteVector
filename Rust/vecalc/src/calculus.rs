6fcb862d41f78751bb1fbfc0d06bdcf418f0accf
pya-h <p4ya.py@gmail.com>
2024-11-02 02:46:29 +0000

    [impl] basic analyzation mechanis

--- Rust/vecalc/src/calculus.rs
+++ Rust/vecalc/src/calculus.rs
@@ -15,4 +15,23 @@ impl Analyzer {
     pub fn get(&mut self) -> &mut memory::Memory {
         &mut self.memory
     }
+
+    fn anaalyze(instruction: &String) {
+        let mut tokens = instruction.split_whitespace();
+        let first = match tokens.next() {
+            Some(token) => token,
+            None => ""
+        };
+        if first.is_empty() {
+            print!("Invalid Instruction!");
+            return ();
+        }
+        if let Some(first_char)  = first.chars().next() {
+            if first_char.is_alphabetic() {
+                // define a new vector
+            }
+            
+        }
+        
+    }
 }
