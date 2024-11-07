9eb46080dfe6477bfed640a3f80b6219425a7d2e
pya-h <p4ya.py@gmail.com>
2024-11-07 18:42:11 +0000

    add an enum to hold different kinds of values in app - also implemented the analyzer srructure.

--- Rust/vecalc/src/calculus.rs
+++ Rust/vecalc/src/calculus.rs
@@ -1,6 +1,13 @@
 pub mod memory;
 pub mod vector;
 pub mod matrix;
+pub enum Term {
+    Vector(vector::Vector),
+    Matrix(matrix::Matrix),
+    Scalar(f64),
+    Null,
+    Wtf(String)
+}
 pub struct Analyzer {
     memory: memory::Memory,
 }
@@ -16,10 +23,26 @@ impl Analyzer {
         &mut self.memory
     }
 
-    fn anaalyze(instruction: &String) {
+    fn evaluate(tokens: Vec<&str>) -> Term {
+        for token in &tokens {
+            if token == &"(" {
+                // TODO: extract vector
+            } else if token == &"[" {
+                // TODO: extract matrix
+            } else {
+                // TODO: probably a pre-defined vector
+            }
+        }
+        Term::Null
+    }
+    pub fn anaalyze(instruction: &String) {
         let tokens: Vec<&str> = instruction.split_whitespace().collect();
-        let terms = tokens.len();
+        let terms_count: usize = tokens.len();
 
-        
+        if terms_count > 2 {
+            if tokens[1] == "=" {
+
+            }
+        }
     }
 }
