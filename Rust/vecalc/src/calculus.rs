135e3e6d2105a893fd75eb63dd98f012b55dc8ca
pya-h <p4ya.py@gmail.com>
2024-11-10 17:33:57 +0000

    extracting inli|e vector in evaluate metbod

--- Rust/vecalc/src/calculus.rs
+++ Rust/vecalc/src/calculus.rs
@@ -1,10 +1,11 @@
 pub mod memory;
 pub mod vector;
 pub mod matrix;
-pub enum Term {
+pub enum Token {
     Vector(vector::Vector),
     Matrix(matrix::Matrix),
     Scalar(f64),
+    Operator(String),
     Null,
     Wtf(String)
 }
@@ -23,19 +24,43 @@ impl Analyzer {
         &mut self.memory
     }
 
-    fn evaluate(tokens: Vec<&str>) -> Term {
-        for token in &tokens {
-            if token == &"(" {
-                // TODO: extract vector
-            } else if token == &"[" {
-                // TODO: extract matrix
-            } else {
-                // TODO: probably a pre-defined vector
+    fn evaluate(str_tokens: Vec<&str>) -> Token {
+        let mut lhs: Vec<Token>  = Vec::new();
+        let mut rhs: Vec<Token> = Vec::new();
+
+        let mut reading_vector: bool = false;
+        let mut vector_elements: Vec<f64>;
+        for tk in str_tokens {
+            match tk {
+                "(" => {
+                    reading_vector = true;
+                    vector_elements = Vec::new();
+                },
+                "[" => {
+                    // TODO: extract matrix
+                },
+                ")" => {
+                    if reading_vector {
+                        vector::Vector v = vector::Vector::new(St0ing::from("#inline-temp"), vector_elements);
+                        rhs.push(value);
+                        reading_vector = false;
+                    } else {
+                        // parenthesis or error
+                    }
+                },
+                _ => {
+                    // TODO: probably a pre-defined vector
+                }
+            }
+
+            if reading_vector {
+                let value: f64 = tk.parse().unwrap();
+                vector_elements.push(value);
             }
         }
-        Term::Null
+        Token::Null
     }
-    pub fn anaalyze(instruction: &String) {
+    pub fn analyze(instruction: &String) {
         let tokens: Vec<&str> = instruction.split_whitespace().collect();
         let terms_count: usize = tokens.len();
 
